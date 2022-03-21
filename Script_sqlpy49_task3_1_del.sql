adca0b5ca149b0d70a1febbd6e345d54691356d4
Sergey Kievskiy <kievsan@mail.ru>
2022-03-21 11:14:04 +0300

    Edit files

--- Script_sqlpy49_task3_1_del.sql
+++ Script_sqlpy49_task3_1_del.sql
@@ -5,12 +5,12 @@ set schema 'sqlpy49_task3_1';
 
 --УДАЛИМ ОТНОШЕНИЯ:
 
-drop table genre_into_artist;
-drop table artist_into_album;
-drop table track_into_collection;
+drop table if exists genre_into_artist;
+drop table if exists artist_into_album;
+drop table if exists track_into_collection;
 
-drop table music_genres;
-drop table music_tracks;
-drop table music_collections;
-drop table music_artists;
-drop table music_albums;
+drop table if exists music_genres;
+drop table if exists music_tracks;
+drop table if exists music_collections;
+drop table if exists music_artists;
+drop table if exists music_albums;
